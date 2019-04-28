ccf873175e1758882aaecf624e27377854e03e47
kevmac64 <k_mac87@live.com>
2019-04-28 16:29:13 -0500

    Added a crude but working Pizza Factory that takes a dictionary and builds a decorated pizza based on values

--- src/main-pizza.py
+++ src/main-pizza.py
@@ -1,10 +1,35 @@
 from src.classes_pizza import PizzaType, PizzaElement
+from src.pizza_factory import PizzaFactory
 
-cheese = PizzaType('Cheese', 7)
+cheese = PizzaType('Cheese', 7.99)
 
 print(cheese)
 
-extra_cheese = PizzaElement('Extra Cheese', .40, cheese)
-gluten_free = PizzaElement('Gluten Free', .25, extra_cheese)
+extra_cheese = PizzaElement('Extra Cheese', .30, cheese)
+gluten_free = PizzaElement('Gluten Free', 3.99, extra_cheese)
 print(extra_cheese, extra_cheese.show_cost)
 print(gluten_free, gluten_free.show_cost)
+
+
+# Now try with a beginning factory
+print('With factory')
+kitchen = PizzaFactory()
+order = {'base': 'cheese',
+         'add': 'extra cheese',
+         'crust': ''}
+
+my_pizza = kitchen.build_a_pizza(order)
+print(my_pizza, my_pizza.show_cost)
+
+
+order = {'base': 'sausage',
+         'add': '',
+         'crust': 'gf'}
+my_pizza = kitchen.build_a_pizza(order)
+print(my_pizza, my_pizza.show_cost)
+
+order = {'base': 'vegetarian',
+         'add': 'extra cheese',
+         'crust': ''}
+my_pizza = kitchen.build_a_pizza(order)
+print(my_pizza, my_pizza.show_cost)
---
+++ src/pizza_factory.py
@@ -0,0 +1,46 @@
+from src.classes_pizza import Pizza, PizzaType, PizzaElement
+
+
+class PizzaFactory(Pizza):
+
+    def __init__(self):
+        self.costs = {'cheese': 7.99,
+                      'peperoni': 8.99,
+                      'sausage': 8.99,
+                      'vegetarian': 8.99,
+                      'combination': 9.99,
+                      'extra': .3,
+                      'remove': -.25,
+                      'gf': 3.99}
+        self.base = PizzaType
+        self.decorated = PizzaElement
+        self.add_on = PizzaElement
+        self._list = []
+
+    def build_a_pizza(self, description):
+        if description['base'] == 'cheese':
+            self.base = PizzaType('Cheese', self.costs['cheese'])
+        elif description['base'] == 'peperoni':
+            self.base = PizzaType('Peperoni', self.costs['peperoni'])
+        elif description['base'] == 'sausage':
+            self.base = PizzaType('Sausage', self.costs['sausage'])
+        elif description['base'] == 'vegetarian':
+            self.base = PizzaType('Vegetarian', self.costs['vegetarian'])
+        elif description['base'] == 'combination':
+            self.base = PizzaType('Combination', self.costs['combination'])
+
+        # need to add a size builder
+
+
+        # this part needs work with a loop for other adds
+        if description['add'] == 'extra cheese':
+            self.decorated = PizzaElement('Extra Cheese', self.costs['extra'], self.base)
+
+        # need to implement all crusts
+        if description['crust'] == 'gf':
+            self.decorated = PizzaElement('Gluten Free', self.costs['gf'], self.base)
+
+        return self.decorated
+
+
+
