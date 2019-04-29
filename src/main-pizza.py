6457c1b6e9f9ee5a6ecdd49d7170729a999dbec2
kevmac64 <k_mac87@live.com>
2019-04-29 15:19:33 -0500

    Implementing Scott's PizzaFactory tip

--- src/main-pizza.py
+++ src/main-pizza.py
@@ -1,15 +1,4 @@
-from src.classes_pizza import PizzaType, PizzaElement
 from src.pizza_factory import PizzaFactory
-
-print('\nTesting decorator pattern')
-cheese = PizzaType('Cheese', 7.99)
-
-print(cheese)
-
-extra_cheese = PizzaElement('Extra Cheese', .30, cheese)
-gluten_free = PizzaElement('Gluten Free', 3.99, extra_cheese)
-print(extra_cheese, extra_cheese.show_cost)
-print(gluten_free, gluten_free.show_cost)
 
 
 # Now try with a beginning factory
@@ -17,7 +6,8 @@ print('\nWith factory pattern')
 kitchen = PizzaFactory()
 order = {'base': 'cheese',
          'add': 'extra cheese',
-         'crust': ''}
+         'size': 'small',
+         'crust': 'regular'}
 
 my_pizza = kitchen.build_a_pizza(order)
 print(my_pizza, my_pizza.show_cost)
@@ -25,12 +15,28 @@ print(my_pizza, my_pizza.show_cost)
 
 order = {'base': 'sausage',
          'add': '',
-         'crust': 'gf'}
+         'size': 'medium',
+         'crust': 'thin'}
+my_pizza = kitchen.build_a_pizza(order)
+print(my_pizza, my_pizza.show_cost)
+
+order = {'base': 'sausage',
+         'add': '',
+         'size': 'large',
+         'crust': 'thin'}
+my_pizza = kitchen.build_a_pizza(order)
+print(my_pizza, my_pizza.show_cost)
+
+order = {'base': 'sausage',
+         'add': '',
+         'size': 'large',
+         'crust': 'gluten-free'}
 my_pizza = kitchen.build_a_pizza(order)
 print(my_pizza, my_pizza.show_cost)
 
 order = {'base': 'vegetarian',
          'add': 'extra cheese',
-         'crust': ''}
+         'size': 'slice',
+         'crust': 'cheese filled'}
 my_pizza = kitchen.build_a_pizza(order)
 print(my_pizza, my_pizza.show_cost)
--- src/pizza_factory.py
+++ src/pizza_factory.py
@@ -4,43 +4,90 @@ from src.classes_pizza import Pizza, PizzaType, PizzaElement
 class PizzaFactory(Pizza):
 
     def __init__(self):
-        self.costs = {'cheese': 7.99,
-                      'pepperoni': 8.99,
-                      'sausage': 8.99,
-                      'vegetarian': 8.99,
-                      'combination': 9.99,
-                      'extra': .3,
-                      'remove': -.25,
-                      'gf': 3.99}
-        self.base = PizzaType
-        self.decorated = PizzaElement
-        self.add_on = PizzaElement
+        self.costs = {
+            'cheese': 7.99,
+            'pepperoni': 8.99,
+            'sausage': 8.99,
+            'vegetarian': 8.99,
+            'combination': 9.99,
+            'extra': .3,
+            'remove': -.25,
+            'gf': 3.99
+        }
+        self.names = {
+            'cheese': 'Cheese',
+            'pepperoni': 'Pepperoni',
+            'sausage': 'Sausage',
+            'vegetarian': 'Vegetarian',
+            'combination': 'Combination',
+            'special': 'Special'
+        }
+        self.sizes = {
+            'slice': 'Slice',
+            'individual': 'Individual',
+            'small': 'Small',
+            'medium': 'Medium',
+            'large': 'Large'
+        }
+        self.size_costs = {
+            'slice': -4.99,
+            'individual': -2.99,
+            'small': -1.99,
+            'medium': 0,
+            'large': 1.99
+        }
+        self.adds = {
+            'extra cheese': 'Extra Cheese',
+            'extra veggies': 'Extra Veggies',
+            'onions': 'Onions',
+            'olives': 'Olives',
+            'extra sauce': 'Extra Sauce',
+            'double meat': 'Double Meat',
+            'banana peppers': 'Banana Peppers'
+        }
+        self.remove = {
+            'cheese': 'hold Cheese',
+            'onions': 'hold Onions',
+            'olives': 'hold Olives',
+            'sauce': 'hold Sauce',
+            'meat': 'hold Meat',
+            'peppers': 'hold Peppers'
+        }
+        self.crusts = {
+            'thin': 'Thin Crust',
+            'regular': 'Regular Crust',
+            'gluten-free': 'Gluten-Free',
+            'thick': 'Thick Crust',
+            'cheese filled': 'Cheese Filled Crust'
+        }
+        self.crusts_costs = {
+            'thin': 0,
+            'regular': 0,
+            'gluten-free': 4.99,
+            'thick': 1.00,
+            'cheese filled': 1.50
+        }
+        # not sure if this is needed
         self._list = []
 
     def build_a_pizza(self, description):
-        if description['base'] == 'cheese':
-            self.base = PizzaType('Cheese', self.costs['cheese'])
-        elif description['base'] == 'pepperoni':
-            self.base = PizzaType('Pepperoni', self.costs['peperoni'])
-        elif description['base'] == 'sausage':
-            self.base = PizzaType('Sausage', self.costs['sausage'])
-        elif description['base'] == 'vegetarian':
-            self.base = PizzaType('Vegetarian', self.costs['vegetarian'])
-        elif description['base'] == 'combination':
-            self.base = PizzaType('Combination', self.costs['combination'])
+        base_name = description['base']
+        size = description['size']
+        crust = description['crust']
 
-        # need to add a size builder
-
+        # start the building with a base
+        the_base_pizza = PizzaType(self.names[base_name], self.costs[base_name])
+        # then add the size
+        the_sized_pizza = PizzaElement(self.sizes[size], self.size_costs[size], the_base_pizza)
+        # then add the crust choice
+        pizza_with_crust = PizzaElement(self.crusts[crust], self.crusts_costs[crust], the_sized_pizza)
 
         # this part needs work with a loop for other adds
-        if description['add'] == 'extra cheese':
-            self.decorated = PizzaElement('Extra Cheese', self.costs['extra'], self.base)
+        #   decorated_pizza = PizzaElement('Extra Cheese', self.costs['extra'], pizza_with_crust)
 
-        # need to implement all crusts
-        if description['crust'] == 'gf':
-            self.decorated = PizzaElement('Gluten Free', self.costs['gf'], self.base)
+        the_finished_pizza = pizza_with_crust
 
-        return self.decorated
+        return the_finished_pizza
 
 
 
